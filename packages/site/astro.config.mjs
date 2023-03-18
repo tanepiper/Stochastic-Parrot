6252452089a962804cb284fe8ff8eb79d7d84cbf
Tane Piper <tane.piper@inter.ikea.com>
2023-03-18 20:23:07 +0100

    Remove old dep

--- packages/site/astro.config.mjs
+++ packages/site/astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
-import netlify from '@astrojs/netlify/functions'
 
 // https://astro.build/config
 export default defineConfig({
-    outDir: '../../docs',
     output: 'static',
     integrations: [svelte()],
 });
