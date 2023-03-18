cea349639bb34878c2921edd30314fbc15262398
Tane Piper <tane.piper@inter.ikea.com>
2023-03-18 20:30:17 +0100

    Fixing docs

--- packages/site/astro.config.mjs
+++ packages/site/astro.config.mjs
@@ -3,6 +3,7 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
+    outDir: '../../docs',
     output: 'static',
     integrations: [svelte()],
 });
