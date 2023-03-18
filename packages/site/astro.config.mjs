84b1acb6fa5ebe6d79a30663d6d756a6234bc247
Tane Piper <tane.piper@inter.ikea.com>
2023-03-18 17:11:14 +0100

    attempt site deploy

--- packages/site/astro.config.mjs
+++ packages/site/astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
-import netlify from '@astrojs/netlify';
+import netlify from '@astrojs/netlify/functions'
 
 // https://astro.build/config
 export default defineConfig({
