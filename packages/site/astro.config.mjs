0a80071821d1c033f50c0c26ce402a4a5f5d8562
Tane Piper <tane.piper@inter.ikea.com>
2023-03-18 16:57:17 +0100

    first attempt at page

--- packages/site/astro.config.mjs
+++ packages/site/astro.config.mjs
@@ -4,6 +4,7 @@ import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
+    output: 'server',
     integrations: [svelte()],
     adapter: netlify(),
 });
