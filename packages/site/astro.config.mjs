df927fd83e77d31723110b5dd197cca4a84ea7cf
Tane Piper <tane.piper@inter.ikea.com>
2023-03-20 21:18:33 +0100

    optimise images

--- packages/site/astro.config.mjs
+++ packages/site/astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
+import image from '@astrojs/image';
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'static',
-    integrations: [svelte()],
+  output: 'static',
+  integrations: [
+    svelte(),
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp',
+    }),
+  ],
 });
