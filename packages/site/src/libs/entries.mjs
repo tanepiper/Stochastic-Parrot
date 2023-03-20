f9939265a56239bbfa6f51b6920b273479547ed1
Tane Piper <tane.piper@inter.ikea.com>
2023-03-20 21:21:17 +0100

    sort order

--- packages/site/src/libs/entries.mjs
+++ packages/site/src/libs/entries.mjs
@@ -1,17 +1,15 @@
-
 /**
  * Creates a loader manager for all entries.
  */
 export function createEntriesLoader() {
+  let entries = [];
 
-    let entries = []
-
-    /**
-     * @param {number=} start
-     * @param {number=} end
-     * @param {string=} sortBy
-     * @returns {Promise<[]>}
-     */
+  /**
+   * @param {number=} start
+   * @param {number=} end
+   * @param {string=} sortBy
+   * @returns {Promise<[]>}
+   */
   async function loadEntries(start = 0, end = 0, sortBy = 'created') {
     const entryFiles = await import.meta.glob('../../public/entries/*.json', {
       eager: true,
@@ -19,23 +17,25 @@ export function createEntriesLoader() {
     let sorted = Object.values(entryFiles ?? {})
       .map((e) => e.default)
       .sort((a, b) => (a[sortBy] > b[sortBy] ? -1 : 1));
-      entries = [...sorted];
+    entries = [...sorted];
 
     if (end > 0) {
-        sorted = sorted.slice(start, end);
+      sorted = sorted.slice(start, end);
     }
     return sorted;
   }
 
   async function getDallEImageFilenames() {
-      const imageFiles = await import.meta.glob('../../public/dall-e/*.png', {
-          eager: true,
-      });
-      return Object.keys(imageFiles).map((e) => e.replace('../../public/dall-e/', ''));
+    const imageFiles = await import.meta.glob('../../public/dall-e/*.png', {
+      eager: true,
+    });
+    return Object.keys(imageFiles)
+      .map((e) => e.replace('../../public/dall-e/', ''))
+      .sort((a, b) => (a > b ? -1 : 1));
   }
 
   function getEntries() {
-      return entries;
+    return entries;
   }
 
   return { loadEntries, getEntries, getDallEImageFilenames };
