4c498c7abf912d06040d74fe0743968ae329d5de
Tane Piper <tane.piper@inter.ikea.com>
2023-03-21 13:50:34 +0100

    refactor and cleanup

--- packages/stocastic-parrot/src/openai.mjs
+++ packages/stocastic-parrot/src/openai.mjs
@@ -8,6 +8,16 @@ const debugMode = process.env.DEBUG_MODE === 'true';
 const GPT_MODEL = 'gpt-4'; //'gpt-4';
 const MAX_TOKENS = 500; //200;
 
+const DEFAULT_CHAT_OPTIONS = {
+  frequency_penalty: 1,
+  presence_penalty: 1,
+  temperature: randomNumber(true),
+  max_tokens: MAX_TOKENS,
+  model: GPT_MODEL,
+};
+
+const DEFAULT_IMAGES_OPTIONS = { n: 1, size: '1024x1024' };
+
 /**
  * @typedef {object} ChatOptions
  * @property {number} frequency_penalty
@@ -35,21 +45,17 @@ export function createOpenAIInstance(apiKey) {
   const apiInstance = new OpenAIApi(configuration);
 
   /**
-   * Create a chat completion
-   * @param {string=} prompt
-   * @param {ChatOptions=} options
+   * Takes an optional prompt and returns a response from the OpenAI API chat endpoint. The function provides
+   * some sensible defaults for the options object that give a reasonably conversational response, even if no
+   * prompt is provided.
+   *
+   * The return value is an RxJS Observable that can be subscribed to with the data response from the API.
+   *
+   * @param {string=} prompt Optional prompt to start the conversation
+   * @param {ChatOptions=} options Options for the chat endpoint for the quality of the response
    * @returns import('rxjs').Observable<CreateChatCompletionResponse>
    */
-  function getChat(
-    prompt = '',
-    options = {
-      frequency_penalty: 1,
-      presence_penalty: 1,
-      temperature: randomNumber(true),
-      max_tokens: MAX_TOKENS,
-      model: GPT_MODEL,
-    }
-  ) {
+  function getChat(prompt = '', options = DEFAULT_CHAT_OPTIONS) {
     return from(
       apiInstance.createChatCompletion({
         ...options,
@@ -62,16 +68,22 @@ export function createOpenAIInstance(apiKey) {
   }
 
   /**
-   * Return an object containing links to images
-   * @param {string=} prompt
-   * @param {ImagesOptions=} options
+   * Takes an optional prompt and returns a response from the OpenAI API images endpoint. The function provides
+   * some default options for the number of images and size of the images.
+   *
+   * For this API endpoint, the prompt is required but by default is set to a single space character.
+   *
+   * The return value is an RxJS Observable that can be subscribed to with the data response from the API.
+   *
+   * @param {string=} prompt Optional prompt to generate an image from
+   * @param {ImagesOptions=} options Options for the images endpoint 
    * @returns import('rxjs').Observable<ImageResponse>
    */
-  function getImages(prompt = ' ', options = { n: 1, size: '1024x1024' }) {
+  function getImages(prompt = ' ', options = DEFAULT_IMAGES_OPTIONS) {
     return from(
       apiInstance.createImage({
         prompt,
-       ...options
+        ...options,
       })
     ).pipe(
       retry({ count: 3, delay: 1000 }),
