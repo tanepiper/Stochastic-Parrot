6a2a77b7c9f1eff0327eede33159f3c51545f119
Tane Piper <tane.piper@inter.ikea.com>
2023-03-21 22:05:12 +0100

    Support multiple image upload

--- packages/stocastic-parrot/src/dall-e.mjs
+++ packages/stocastic-parrot/src/dall-e.mjs
@@ -1,48 +1,104 @@
 #!/usr/bin/env node
-
 import dotenv from 'dotenv';
 import path from 'node:path';
 import sharp from 'sharp';
 import minimist from 'minimist';
-import { from } from 'rxjs';
-import { map, switchMap, catchError, finalize } from 'rxjs/operators';
+import { from, of } from 'rxjs';
+import {
+  map,
+  switchMap,
+  catchError,
+  finalize,
+  concatMap,
+  tap,
+  scan,
+  toArray,
+} from 'rxjs/operators';
 import { createMastodonClient } from './mastodon.mjs';
 import { createOpenAIInstance } from './openai.mjs';
 
+/**
+ * A script that runs the Dall-E image generation model from OpenAI and posts the result to Mastodon,
+ * the image is also saved to the site public folder as a `.webp` for use in the site.
+ *
+ * @file dall-e.js
+ * @author Tane Piper <me@tane.dev>
+ * @license MIT
+ * @version 0.0.1
+ */
+
 dotenv.config();
 
-const { _ } = minimist(process.argv.slice(2));
+const { _, ...opts } = minimist(process.argv.slice(2));
 let prompt = _?.[0] ?? ' '; // This should be an empty space
+console.log(opts, prompt);
+if (opts?.help) {
+  console.log(`Usage: dall-e.js [prompt] <options>`);
+  console.log(`Options:`);
+  console.log(`  --help             Show this help message`);
+  console.log(`  --mastodonToken    Mastodon access token`);
+  console.log(`  --openAIToken      OpenAI access token`);
+  console.log(`  --imageQuality     Image quality for WebP file, default 80`);
+  console.log(
+    `  --imageSize        Image size for Dall-E model, default 1024x1024`
+  );
+  console.log(`  --numImages        Number of images to generate, default 1`);
+  process.exit(0);
+}
 
-const openAI = createOpenAIInstance(process.env.OPENAI_API_KEY);
-const mastodon = createMastodonClient(process.env.MASTODON_ACCESS_TOKEN);
+const OPEN_API_KEY = opts?.openAIToken ?? process.env.OPENAI_API_KEY;
+const MASTODON_ACCESS_TOKEN =
+  opts?.mastodonToken ?? process.env.MASTODON_ACCESS_TOKEN;
+
+const webpFileQuality = opts?.imageQuality ?? 80;
+const dalleImageSize = opts?.imageSize ?? '512x512';
+const dalleNumberOfImages = opts?.numImages ?? 2;
+
+const openAI = createOpenAIInstance(OPEN_API_KEY);
+const mastodon = createMastodonClient(MASTODON_ACCESS_TOKEN);
 const filePath = path
   .resolve(`${import.meta.url}`, '..', '..', '..', 'site', 'public', 'dall-e')
   .split(':')[1];
 
+/**
+ * Subscribe to the Observable result of the OpenAI API call, then pipe the response through a series of
+ * RxJS operators to get the image URL, download the image, convert it to a webp, save it to the site
+ * public folder, then post it to Mastodon.
+ */
 openAI
-  .getImages(prompt)
+  .getImages(prompt, { n: dalleNumberOfImages, size: dalleImageSize })
   .pipe(
-    map((response) => {
-      const { url } = response?.data?.[0] ?? '';
-      if (!url) {
-        throw new Error('No images from OpenAI');
-      }
-      return { url, created: response.created };
+    switchMap((response) => {
+      return from(
+        response.data.map((data) => {
+          return { created: response.created, url: data.url };
+        })
+      ).pipe(
+        concatMap(({ url, created }) =>
+          from(fetch(url).then((res) => res.arrayBuffer())).pipe(
+            map((buffer) => ({
+              buffer: Buffer.from(buffer),
+              created,
+            }))
+          )
+        ),
+        concatMap(({ buffer, created }, index) =>
+          from(
+            sharp(buffer)
+              .webp({ quality: webpFileQuality })
+              .toFile(`${filePath}/${created}.${index + 1}.webp`)
+          ).pipe(map(() => `${filePath}/${created}.${index + 1}.webp`))
+        ),
+        concatMap((imagePath) => mastodon.postMedia(imagePath)),
+        toArray()
+      );
     }),
-    switchMap(({ url, created }) =>
-      from(fetch(url).then((res) => res.arrayBuffer())).pipe(
-        map((buffer) => ({ buffer: Buffer.from(buffer), created }))
-      )
-    ),
-    switchMap(({ buffer, created }) =>
-      from(
-        sharp(buffer)
-          .webp({ quality: 80 })
-          .toFile(`${filePath}/${created}.webp`)
-      ).pipe(map(() => `${filePath}/${created}.webp`))
-    ),
-    switchMap((imagePath) => from(mastodon.postMedia(imagePath, !!prompt))),
+
+    switchMap((media) => {
+      const status = prompt !== ' ' ? `💬` : `🦜`;
+      return mastodon.sendToots(`${status}`, media);
+    }),
+
     map((tootUrl) => {
       if (!tootUrl) {
         throw new Error('No tool URL returned from Mastodon');
