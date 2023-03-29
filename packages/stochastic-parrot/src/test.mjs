7b96e1e7f31709760a1824ca69fab726cce36cb6
Tane Piper <tane.piper@inter.ikea.com>
2023-03-29 20:45:10 +0200

    Improve html for header

--- packages/stochastic-parrot/src/test.mjs
+++ packages/stochastic-parrot/src/test.mjs
@@ -1,14 +1,24 @@
 #!/usr/bin/env node
 
 import dotenv from 'dotenv';
+import minimist from 'minimist';
 import { tap } from 'rxjs/operators';
 import { createOpenAIInstance } from './lib/openai.mjs';
 
 dotenv.config();
 
+const { _, ...opts } = minimist(process.argv.slice(2));
+let prompt = _?.[0] ?? '';
+
 const openAI = createOpenAIInstance(process.env.OPENAI_API_KEY);
 
 openAI
-  .getChat('', {}, ``)
-  .pipe(tap((result) => console.log(JSON.stringify(result, null, 2))))
+  .getChat(prompt, { max_tokens: 1000, temperature: 0.2 })
+  .pipe(
+    tap((result) =>
+      console.log(
+        result?.choices?.[0]?.message?.content ?? ''
+      )
+    )
+  )
   .subscribe();
